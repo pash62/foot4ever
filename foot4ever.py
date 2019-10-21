3c0bf21d2898e5c0109cca553d406babdd1fd9c9
pshadkami <pshadkami@factset.com>
2019-10-22 00:29:34 +0200

    Fix dev, prod case sensitivitiy

--- foot4ever.py
+++ foot4ever.py
@@ -419,7 +419,7 @@ class Foot4Ever():
     Manages everything about weekly foot sessions
     """
     def __init__(self):
-        self.token = os.getenv("TOKEN")
+        self.token = os.getenv('TOKEN')
         updater = Updater(token=self.token, use_context=True)
         self.bot = Bot(self.token)
 
@@ -441,17 +441,17 @@ class Foot4Ever():
         self.run(updater)
 
     def run(self, updater):
-        mode = os.getenv("MODE")
-        if mode == "dev":
+        mode = os.getenv('MODE')
+        if mode == 'DEV':
             updater.start_polling()
             updater.idle()
-        elif mode == "prod":
-            port = int(os.environ.get("PORT", "8443"))
-            app_name = os.environ.get("APP_NAME")
-            updater.start_webhook(listen="0.0.0.0", port=port, url_path=self.token)
+        elif mode == 'PROD':
+            port = int(os.environ.get('PORT', '8443'))
+            app_name = os.environ.get('APP_NAME')
+            updater.start_webhook(listen='0.0.0.0', port=port, url_path=self.token)
             updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(app_name, self.token))
         else:
-            logger.error("Invalid mode")
+            logger.error('Invalid mode')
             sys.exit(1)
 
     @WithLogError
