109626e8602f8e822f9172b60fbf6e681f4585da
Sacha <p_shadkami@hotmail.com>
2019-10-27 18:04:53 +0100

    use S3 to keep match info file

--- foot4ever.py
+++ foot4ever.py
@@ -767,11 +767,26 @@ class Foot4Ever():
         """
         loads match date and participants
         """
-        self.match_info_path = os.path.join(os.path.split(__file__)[0], 'match_info.txt')
-        if not os.path.exists(self.match_info_path):
+        self.access_key = os.getenv('CLOUDCUBE_ACCESS_KEY_ID')
+        self.secret_key = os.getenv('CLOUDCUBE_SECRET_ACCESS_KEY')
+        self.url = os.getenv('CLOUDCUBE_URL')
+        self.cube_name = self.url.split('/')[-1]
+        self.bucket_name = url.split('https://')[-1].split('.')[0]
+        self.s3 = boto3.client('s3', aws_access_key_id=self.access_key, aws_secret_access_key=self.secret_key)
+        self.match_info_s3 = os.path.join(self.cube_name, 'match_info.txt')
+        self.match_info = os.path.join(os.path.split(__file__)[0], 'match_info.txt')
+        
+        try:
+            self.s3.download_file(self.bucket_name, self.match_info_s3, self.match_info)
+            print("Upload Successful")
+        except FileNotFoundError:
+            print("The file was not found")
+            return
+        except NoCredentialsError:
+            print("Credentials not available")
             return
 
-        with open(self.match_info_path, 'r') as f:
+        with open(self.match_info, 'r') as f:
             content = json.load(f)
         if content:
             self.init_dates(date = content['date'], center_index = content['center_index'])
@@ -789,8 +804,17 @@ class Foot4Ever():
             if user.order_id<0:
                 continue
             content['cur_players'].append(user.id if user.id>0 else user.user_name)
-        with open(self.match_info_path, 'w') as f:
+            
+        with open(self.match_info, 'w') as f:
             f.write(json.dumps(content))
+                
+        try:
+            self.s3.upload_file(file_path, self.bucket_name, self.match_info_s3)
+            print("Upload Successful")
+        except FileNotFoundError:
+            print("The file was not found")
+        except NoCredentialsError:
+            print("Credentials not available")
 
     @WithLogError
     def show_add_forbidden_player_keyboard(self, update, context):
