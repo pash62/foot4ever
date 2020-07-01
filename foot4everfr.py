87249f7cf39330b4b7af6051b230bb2f19b26923
pash62 <p_shadkami@hotmail.com>
2020-07-01 13:04:14 +0200

    Update foot4everfr.py
    Fix sending the message

--- foot4everfr.py
+++ foot4everfr.py
@@ -850,7 +850,7 @@ class Foot4Ever():
             days = {0:'Monday', 1:'Tuesday', 2:'Wednesday', 3:'Thursday', 4:'Friday', 5:'Saturday', 6:'Sunday'}
             next_date = (datetime.now(pytz.timezone('Europe/Paris')) + timedelta(days=45)).weekday()
             #if weekday in (0, 1, 2): # Monday, Tuesday, Wednesday
-            bot.edit_message_text(text=Msg.next_potential_date.format(days[next_date]), message_id=query.message.message_id, chat_id=query.message.chat_id)
+            update.message.reply_text(text=Msg.next_potential_date.format(days[next_date]))
 
 
 def main():
